1f476364b13dbf6183c3b595f02e152412133408
Luis Miguel Cruz Gareca <lc109070@gmail.com>
2024-06-07 08:07:33 -0400

    empesamos

---
+++ src/stores/indes.ts
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia'
+
+import http from '@/plugins/axios'
+import router from '@/router'
+import { getTokenFromLocalStorage } from '@/assets/helpers'
+
+
+const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: localStorage.getItem('user') || '',
+    token: getTokenFromLocalStorage(),
+    returnUrl: null || ''
+  }),
+  getters: {},
+  actions: {
+    async login(usuario: string, clave: string) {
+      await http.post('auth/login', { usuario, clave }).then((response) => {
+        this.user = response.data.usuario
+        this.token = response.data.access_token
+
+        localStorage.setItem('user', this.user || '')
+        localStorage.setItem('token', this.token || '')
+
+        router.push(this.returnUrl || '/')
+      })
+    },
+    logout() {
+      localStorage.clear()
+      this.$reset()
+      router.push('login')
+    }
+  }
+})
+
+export { useAuthStore }
